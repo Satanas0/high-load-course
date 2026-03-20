b867038f22eeefbd0800f77936ebd67ca7d7d8fb
Satanas0 <113409366+Satanas0@users.noreply.github.com>
2026-03-20 04:33:38 +0300

    Update PaymentExternalServiceImpl.kt

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -100,7 +100,7 @@ class PaymentExternalSystemAdapterImpl(
     }
 
     private fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long, transactionId: UUID, attempt: Long) {
-        if (now() + requestAverageProcessingTime > deadline || attempt >= MAX_ATTEMPTS) {
+        if (now() >= deadline || attempt >= MAX_ATTEMPTS) {
             metricsService.incrementCounter("payment_failed_external", "Failed external requests")
             val currentTime = now()
             dbScope.launch {
@@ -254,7 +254,7 @@ class PaymentExternalSystemAdapterImpl(
         val p95 = latencyProfile.quantile(0.95)
         val avg = requestAverageProcessingTime.coerceAtLeast(20L)
         val target = max(avg, p95)
-        val adaptive = target.coerceAtLeast(150L).coerceAtMost(800L)
+        val adaptive = target.coerceAtLeast(150L)
         val boundedByDeadline = (timeLeft - 50L).coerceAtLeast(500L)
         return adaptive.coerceAtMost(boundedByDeadline)
     }
